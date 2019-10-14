7950b5bcde04db141a8ee7c1788105bde639913d
Антон Середа <antonsereda@MacBook-Pro-Anton.local>
2019-10-14 23:24:26 +0300

    to add methods and validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,10 @@
 class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
+  validate :compare
+  def compare
+  	if original_text.downcase == translated_text.downcase
+  		errors.add(:original_text, "Original text and translated text mustn't be equal")
+  	end
+  end
 end
