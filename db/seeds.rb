6bbec4ec1fc336522ac0fd88e39fc0eedce825b5
Vladislav <valdos-liverpool@mail.ru>
2019-10-19 17:25:49 +0300

    add create! and change seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,2 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
+require './lib/seeds/some_class'
 
-source = 'https://1000mostcommonwords.com/1000-most-common-german-words'
-page = Nokogiri::HTML(open(source))
-page.css('.entry-content tr').take(5).each do |a|
-  Card.create(
-    original_text: a.css('td:nth-last-child(2)').text,
-	translated_text: a.css('td:last-child').text  
-  )
-end
