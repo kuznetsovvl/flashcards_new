cab7c77435dd5b5f75e30dcb19b10c7ffd2b8d4f
Vladislav <valdos-liverpool@mail.ru>
2019-10-20 20:03:20 +0300

    new methods

--- lib/seeds/some_class.rb
+++ lib/seeds/some_class.rb
@@ -2,14 +2,22 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
+  SOURCE = 'https://1000mostcommonwords.com/1000-most-common-german-words'
   def scraper_cards
-    source = 'https://1000mostcommonwords.com/1000-most-common-german-words'
-    page = Nokogiri::HTML(open(source))
+    page
+    scraper_table
+  end
+
+  def page
+    page = Nokogiri::HTML(open(SOURCE))
+  end
+
+  def scraper_table
     page.css('.entry-content tr').take(5).each do |a|
       Card.create!(
         original_text: a.css('td:nth-last-child(2)').text,
-        translated_text: a.css('td:last-child').text  
-  	)
+        translated_text: a.css('td:last-child').text
+      )
     end
-  end  
+  end
 end
