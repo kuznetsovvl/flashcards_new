eb44dff4997d76bfb7fc94635c231a4a77403e15
Vladislav <valdos-liverpool@mail.ru>
2019-12-30 16:58:21 +0300

    fixed uniqness validate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
-  it { should validate_uniqueness_of(:email) }
+		let!(:user) { create(:user) }
+  it do 
+  	should validate_uniqueness_of(:email)
+  end
   it { should validate_confirmation_of(:password) }
 end
