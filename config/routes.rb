7c6b9cd0e329f1a1dc6afab1314487310b5e6c8e
kuznetsovvl <valdos-liverpool@mail.ru>
2020-02-09 14:02:10 +0300

    to fix mistakes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'oauths/oauth'
-  get 'oauths/callback'
   get 'welcome/index'
   root 'welcome#index'
   delete 'logout' => 'sessions#destroy', as: :logout
   post 'login' => 'sessions#new', as: :login
   post 'signup' => 'users#new', as: :signup
   get 'sessions/new'
-  get 'oath/oauth'
+
   scope '/oauth' do
     post 'callback' => 'oauths#callback'
     get 'callback' => 'oauths#callback'
-    get ':provider' => 'oauths#oauth', :as => :auth_at_provider
+    get ':provider' => 'oauths#oauth', as: :auth_at_provider
   end
 
   resources :users do
