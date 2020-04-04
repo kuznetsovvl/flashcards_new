cdcea7cfbec2c83afb34a90f36660f57600f193f
kuznetsovvl <valdos-liverpool@mail.ru>
2020-04-04 12:44:59 +0300

    update_random_card

--- app/services/random_card.rb
+++ app/services/random_card.rb
@@ -2,7 +2,9 @@
 
 class RandomCard
   def today_card(user)
-    @current_decks = Deck.where(user_id: user.id).last
-    Card.where(deck_id: @current_decks.id).days_ago.last
- end
+    @current_decks = Deck.where(user_id: user.id).sample
+    unless @current_decks.nil?
+      Card.where(deck_id: @current_decks.id).days_ago.last
+    end
+   end
 end
