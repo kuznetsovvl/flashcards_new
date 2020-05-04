a5b7931ed541d96e5f62ebb61f4b919a73bff6ac
kuznetsovvl <valdos-liverpool@mail.ru>
2020-05-04 18:15:53 +0300

    update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validation tests' do
+    let!(:user) {create :user}
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:email) }
