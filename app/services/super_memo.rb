f612a7efae3e12e286d90a6b471406b7bc50ce71
Vladislav <valdos-liverpool@mail.ru>
2020-06-24 15:09:51 +0300

    added constants

--- app/services/super_memo.rb
+++ app/services/super_memo.rb
@@ -4,6 +4,10 @@ require 'damerau-levenshtein'
 
 class SuperMemo
   QUALITY_RANGE = (1..5).freeze
+  MIN_RATIO_EASINESS = 1.5
+  PERMISSIBLE_QUALITY = 3
+  DEFAULT_FIRST_INTERVAL = 1
+  DEFAULT_SECOND_INTERVAL = 6
 
   attr_reader :card, :quality
 
@@ -13,7 +17,7 @@ class SuperMemo
   end
 
   def call
-    card.easiness = [1.5, card.easiness + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))].max
+    card.easiness = [MIN_RATIO_EASINESS, card.easiness + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))].max
     set_repetitions_counter
     interval_counter
     set_review_time
@@ -23,16 +27,16 @@ class SuperMemo
   private
 
   def set_repetitions_counter
-    quality <= 3 ? card.repetitions = 0 : card.repetitions += 1
+    quality <= PERMISSIBLE_QUALITY ? card.repetitions = 0 : card.repetitions += 1
   end
 
   def interval_counter
     result = if card.repetitions <= 1
-               1
+               DEFAULT_FIRST_INTERVAL
              elsif card.repetitions == 2
-               6
+               DEFAULT_SECOND_INTERVAL
              else
-               card.interval *= card.easiness
+               card.interval * card.easiness
             end
     card.interval = result
   end
